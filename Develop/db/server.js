3dc71f17b13c1528c34afdbc96d90a52d58b0cbe
Megan Bryan <meganmbryan12@gmail.com>
2021-01-25 12:18:51 -0800

    Resolve file paths

--- Develop/db/server.js
+++ Develop/db/server.js
@@ -10,11 +10,11 @@ app.use(express.json());
 var notesArray = [];
 
 app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "notes.html"));
+    res.sendFile(path.join(__dirname, "public/notes.html"));
 });
 
 app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
+    res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
 app.get("/api/notes", function(req, res) {
