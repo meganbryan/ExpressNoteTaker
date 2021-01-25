ced27f7d03a2872ff4a8410aae83c9c4a1541a82
Megan Bryan <meganmbryan12@gmail.com>
2021-01-25 12:00:41 -0800

    Add server.js with boilerplate

---
+++ Develop/db/server.js
@@ -0,0 +1,37 @@
+var express = require("express");
+const { fstat } = require("fs");
+var path = require("path");
+var app = express();
+var PORT = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+var notesArray = [];
+
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "notes.html"));
+});
+
+app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/api/notes", function(req, res) {
+    fstat.readFile('db.json', (err, data) => {
+        if (err) throw err;
+        console.log(data);
+    })
+});
+
+app.post("/api/notes/:id", function(req, res) {
+    var newNote = req.body;
+    newNote.routeName = newNote.name.replace(/\s+/g, "").toLowerCase();
+    console.log(newNote);
+    notesArray.push(newNote);
+    res.json(newNote);
+});
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
